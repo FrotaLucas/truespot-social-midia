673da6e8b49b42e8dcb223b786c82db9424175cf
FrotaLucas <lucasdiasfrota@gmail.com>
2024-10-15 23:00:08 +0200

    my first commit

---
+++ TrueSpot/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TrueSpot
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TrueSpot/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TrueSpot
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
