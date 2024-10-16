5b71186854cbb6933436f95e4b905f4c478e5f86
Adam Heiderich <damheid@hotmail.com>
2024-10-16 20:16:33 +0200

    feat: Created a workflow for the Events.

--- TrueSpot/Models/TrueSpotEvent.cs
+++ TrueSpot/Models/TrueSpotEvent.cs
@@ -3,11 +3,9 @@
     public class TrueSpotEvent
     {
         public Guid Id { get; set; }
-
         public Guid CreatorUserId { get; set; }
         public string Title { get; set; } = default!;
         public string Description { get; set; } = default!;
-
         public DateTime StartDate { get; set; } = default!;
         public DateTime? EndDate { get; set; }
     }
---
+++ TrueSpot/Workflows/EventWorkflow.cs
@@ -0,0 +1,21 @@
+﻿using TrueSpot.Models;
+
+namespace TrueSpot.Workflows
+{
+    internal class EventWorkflow
+    {
+        public TrueSpotEvent CreateEvent(TrueSpotUser user, string title, string description, DateTime startdate, DateTime? enddate = null)
+        {
+            // TODO: Save to a database where the ID is created automatically.
+            return new TrueSpotEvent
+            {
+                Id = Guid.NewGuid(),
+                CreatorUserId = user.Id,
+                Title = title,
+                Description = description,
+                StartDate = startdate,
+                EndDate = enddate
+            };
+        }
+    }
+}
